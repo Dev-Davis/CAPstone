f4d3a9a59409352f2520a17ff915c059f7e3fab9
Sean Davis <sean.dev.davis@gmail.com>
2019-07-30 15:21:08 -0500

    profile fix - wip

--- src/Components/MyProfile/MyProfile.js
+++ src/Components/MyProfile/MyProfile.js
@@ -1,51 +1,198 @@
-import React from 'react';
-import firebase from 'firebase/app';
-// import { Link } from 'react-router-dom';
+import React from "react";
+import firebase from "firebase/app";
 
-import ProfileHatsCard from '../ProfileHatCard/ProfileHatCard';
+import ProfileHatsCard from "../ProfileHatCard/ProfileHatCard";
 
-import hatData from '../../helpers/data/hatData';
+import hatData from "../../helpers/data/hatData";
 
-import './MyProfile.scss';
+import userShapes from '../../helpers/propz/userShapes';
+
+import "./MyProfile.scss";
+
+const defaultHatInfo = {
+  name: '',
+  type: '',
+  colorWay: '',
+  description: ''
+};
 
 class Home extends React.Component {
   state = {
     hats: [],
+    newHat: defaultHatInfo,
+    name: '',
+    type: '',
+    colorWay: '',
+    description: ''
+  };
+
+  static propTypes = {
+    users: userShapes.userShapes,
+  }
+
+  /* Changes the state for the strings in each category
+  for adding them to the page */
+
+  stringStateField = (name, e) => {
+    const copyHats = { ...this.state.newHat };
+    copyHats[name] = e.target.value;
+    this.setState({ newHat: copyHats });
+  };
+
+  nameChange = e => this.stringStateField('name', e);
+  typeChange = e => this.stringStateField('type', e);
+  colorWayChange = e => this.stringStateField('colorWay', e);
+  descriptionChange = e => this.stringStateField('description', e);
+  
+// The next four variables sets you form to a set state of controlled or uncontrolled
+  nameChange = (e) => {
+    e.preventDefault();
+    this.setState({ name: e.target.value })
+  } 
+  
+  typeChange = (e) => {
+    e.preventDefault();
+    this.setState({ type: e.target.value })
+  } 
+  
+  colorWayChange = (e) => {
+    e.preventDefault();
+    this.setState({ colorWay: e.target.value })
+  } 
+  
+  descriptionChange = (e) => {
+    e.preventDefault();
+    this.setState({ description: e.target.value })
   }
 
   getHats = () => {
     const { uid } = firebase.auth().currentUser;
-    hatData.getHats(uid)
-      .then(hats => this.setState({hats}))
-      .catch(err => console.error('could not get hats for home', err));
-  }
-
+    hatData
+      .getHats(uid)
+      .then(hats => this.setState({ hats }))
+      .catch(err => console.error("could not get hats for home", err));
+  };
 
   componentDidMount() {
     this.getHats();
   }
 
-  render() {
-    const makeHatCards = this.state.hats.map(hat => (
-      <ProfileHatsCard
-        key={hat.id}
-        hats={hat}
-      />
-    ));
+  submitForm = e => {
+    e.preventDefault();
+    const saveHat = { ...this.state.newHat };
+    saveHat.uid = firebase.auth().currentUser.uid;
+    hatData
+      .postNewHat(saveHat)
+      .then(() => this.getHats())
+      // .catch(err => console.error("unable to post new hat", err));
+      this.setState({ 
+        name: '',
+        type: '',
+        colorWay: '',
+        description: ''
+      })
+  };
 
-    return (
-      <div className="Home col">
-        <h1 className="header-title">Profile Page</h1>
-          <div className="profile-pic">
-            <img src="..." alt="..." />
+  deleteHat = hatId => {
+    hatData
+      .removeHat(hatId)
+      .then(() => this.getHats())
+      .catch(err => console.error("unable to delete the hat", err));
+  };
+
+  fileSelectedHandler = e => {
+    e.preventDefault();
+  };
+  
+  render() {
+    // const { newHat } = this.state;
+    const makeHatCards = this.state.hats.map(hat => (
+      <ProfileHatsCard key={hat.id} hats={hat} deleteHat={this.deleteHat} />
+      ));
+      
+      return (
+      <div className="#">
+        <div className="container">
+          <img src="https://scontent-msp1-1.xx.fbcdn.net/v/t1.0-9/1625624_547718411992311_1237413250_n.jpg?_nc_cat=100&_nc_oc=AQm_nsS7WtfU9RuoqBrMTF29PCOzHt6-CZPofoYhrXCzuixgpclDeHS4PGvJ-Sf5tbQ&_nc_ht=scontent-msp1-1.xx&oh=ef4e2d2355e6e6ae3da59aa529c3f723&oe=5DE68724" alt="profile-pic" className="profilePhoto"/>
+          <form className="col-4 offset-8" onSubmit={this.submitForm}>
+            <div className="uploadTitle">
+              Add a hat...
+            </div>
+              {/* <div className="upload-group">
+                <label htmlFor="uploadFile"></label>
+                <input
+                  type="file"
+                  className="upload-group"
+                  id="uploadFile"
+                  // placeholder="Batman Snapback"
+                  value={newHat.name}
+                  onChange={this.nameChange}
+                />
+              </div> */}
+              <div className="form-group">
+                <label htmlFor="hatName">Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="hatName"
+                  placeholder="Batman Snapback"
+                  value={this.state.name}
+                  onChange={this.nameChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="hatType">Type</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="hatType"
+                  placeholder="Snapback"
+                  value={this.state.type}
+                  onChange={this.typeChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="hatColorWay">Color</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="hatColorWay"
+                  placeholder="Black and Yellow"
+                  value={this.state.colorWay}
+                  onChange={this.colorWayChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="hatDescription">Description</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="hatDescription"
+                  placeholder="A solid black hat with the Batman logo embroidered in the front"
+                  value={this.state.description}
+                  onChange={this.descriptionChange}
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+          </form>
+        </div>
+        <div className="aboutSection text-center col-4 offset-4">
+          <span className="aboutMe">About Me</span>
+          <br></br>
+        {/* </div>
+        <div className="aboutMeDescription col-4 offset-4"> */}
+          I am a lover of hats! This is a website for hat lovers such as myself. Hope you like it also.
+        </div>
+        <div className="container">
+          <div className="d-flex flex-wrap profileCards">
+          {makeHatCards}
           </div>
-          <div className="d-flex flex-wrap">
-            {makeHatCards}
-          </div>
+        </div>
       </div>
     );
   }
 }
 
 export default Home;
-
